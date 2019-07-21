1662f9c9586d01b301a67cc5809fa5be799caf5b
Adrienne <iliu.adrienne@gmail.com>
2019-07-21 12:44:09 -0700

    results are console logging out appropriately

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use(express.json());
 // need folder for public
 app.use(express.static("public"));
 
+var exphbs = require("express-handlebars"); 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 // folder for models
 var db = require("./models");
 
@@ -30,10 +33,24 @@ mongoose.connect("mongodb://localhost/recipes", { useNewUrlParser: true });
 //routes
 //GET route to scrape the website first and save to db
 app.get("/scrape", function(req,res){
-    axios.get("https://www.allrecipes.com/").then(function(response) {
+
+    axios.get("https://www.delish.com/food-news").then(function(response) {
+       
         var $ = cheerio.load(response.data); 
-    })
-})
+        var cards = [];
+        $(".full-item-content").each(function(i, element) {
+            var result = []; 
+            
+            result.title = $(this).children("a").text(); 
+            result.link = $(this).children("a").attr("href");
+
+           cards.push(result.title); 
+           cards.push(result.link);
+            });        console.log(cards);
+        }); res.send("Scrape Complete"); 
+        
+    });
+
 //GET route to get saved data from db
 
 //starting server
